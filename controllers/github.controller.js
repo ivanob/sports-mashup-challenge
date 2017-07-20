f42ecccf814c368c188b2fa3c07a6a1c07626d16
Ivan Obeso <ivan.obeso1@gmail.com>
2017-07-20 02:47:38 +0100

    added config file

--- controllers/github.controller.js
+++ controllers/github.controller.js
@@ -3,6 +3,13 @@ var services = require('../services/github.service');
 module.exports = {
   searchProjects: (name) => {
     console.log('Projects: ' + name)
-    services.getReposRelated('aaa')
+    services.getReposRelated('aaa').
+    then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log('ERROR')
+      //showError(commit, error)
+    })
   }
 }
--- services/github.service.js
+++ services/github.service.js
@@ -1,11 +1,12 @@
 var axios = require('axios');
+var config = require('config');
 
 const URL_GITHUB_API = `https://api.github.com/`
 
 const getReposRelated = keyword => {
   return axios.get(`${URL_GITHUB_API}`, {
     headers: {
-      token: '8cf2fdd1388c52275526b93053d2fc4ec0766904'
+      token: config.github.oauthtoken
     }
   })
 }
