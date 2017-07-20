c20d42f48a92d1fd82d79536964286d6516e88e5
Ivan Obeso <ivan.obeso1@gmail.com>
2017-07-20 02:59:02 +0100

    minor fixes

--- controllers/github.controller.js
+++ controllers/github.controller.js
@@ -3,13 +3,13 @@ var services = require('../services/github.service');
 module.exports = {
   searchProjects: (name) => {
     console.log('Projects: ' + name)
-    services.getReposRelated('aaa').
-    then(response => {
-      console.log(response)
-    })
-    .catch(error => {
-      console.log('ERROR')
-      //showError(commit, error)
-    })
+    services.getReposRelated(name).
+      then(response => {
+        console.log(response.data.items)
+      })
+      .catch(error => {
+        console.log('ERROR')
+        //showError(commit, error)
+      })
   }
 }
--- services/github.service.js
+++ services/github.service.js
@@ -1,10 +1,10 @@
 var axios = require('axios');
 var config = require('config');
 
-const URL_GITHUB_API = `https://api.github.com/`
+const URL_GITHUB_API = `https://api.github.com`
 
 const getReposRelated = keyword => {
-  return axios.get(`${URL_GITHUB_API}`, {
+  return axios.get(`${URL_GITHUB_API}/search/repositories?q=${keyword}`, {
     headers: {
       token: config.github.oauthtoken
     }
